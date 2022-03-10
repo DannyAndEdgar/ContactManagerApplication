b788410717f2922e1e4752eff5dd849423852388
Edgar Ivan Gomez <edgar.i.gomez03@gmail.com>
2022-03-10 16:53:16 -0600

    refactoring code puts functionality in methods

---
+++ src/ContactApp/Contacts.java
@@ -0,0 +1,27 @@
+package ContactApp;
+
+public class Contacts {
+    private String contactName;
+    private String contactNumber;
+
+    public Contacts(String contactName, String contactNumber){
+        this.contactName = contactName;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getContactName(){
+        return this.contactName;
+    }
+
+    public String getContactNumber(){
+        return this.contactNumber;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
